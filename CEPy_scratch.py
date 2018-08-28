c34b68f7fc112381d286161ede9cb1655981aa49
Grant Glazer <gglazer@rmi.org>
2018-08-28 11:06:37 -0600

    Cleaning up eia_923 for EIA 923, using 'for loop' to aggregate monthly energy totals

--- CEPy_scratch.py
+++ CEPy_scratch.py
@@ -3,7 +3,6 @@ from eia_923 import *
 
 pd.set_option('display.max_rows', 200)
 pd.set_option('display.max_columns', 30)
-# test = SetupData(2015)
 test = SetupData()
 
 # test.acquire_ferc()
--- eia_923.py
+++ eia_923.py
@@ -28,7 +28,6 @@ class SetupData(object):
         self.data_path = os.path.abspath('data')
         self.acquire_eia923()
         self.setup_923_monthly()
-        # self.ferc_gross_load = self.setup_ferc_gross_load()
 
     def acquire_eia923(self):
         # download EIA 923 if relevant files are not in 'data' directory
@@ -60,59 +59,26 @@ class SetupData(object):
 
         monthly_energy_full = pd.read_excel(self.data_path + '/EIA923_Schedules_2_3_4_5_M_12_2016_Final_Revision.xlsx',
                                        sheet_name=0, header=5, usecols="A:B,D:G,I,K:N,P,CB:CM,CR:CS")
-        print('dataframe ready! ' + str(datetime.datetime.now().time()))
+        monthly_energy_full = monthly_energy_full.replace(to_replace='.', value=0)
+        # test_df = pd.read_excel(self.data_path + '/EIA923_Schedules_2_3_4_5_M_12_2016_Final_Revision.xlsx',
+        #                                sheet_name=0, header=5, usecols="A,CB:CM,CR")
         monthly_energy = pd.DataFrame(columns=monthly_energy_full.columns)
         new_counter = -1
-        # print(len(df))
         for i in range(len(monthly_energy_full)):
             if monthly_energy_full.iloc[i]['Plant Id'] not in monthly_energy['Plant Id'].values:
                 monthly_energy = monthly_energy.append(monthly_energy_full.loc[i, :])
                 new_counter += 1
             else:
-                # print('New--should be current plant ID ')
-                # print(monthly_energy.iloc[new_counter, monthly_energy.columns.get_loc('Netgen\nJanuary')])
-                # print('Original -- should be the 2nd+ entry with given plant ID ')
-                # print(monthly_energy_full.iloc[i]['Netgen\nJanuary'])
-                # monthly_energy.iloc[new_counter, monthly_energy.columns.get_loc('Netgen\nJanuary')] += \
-                #     float(monthly_energy_full.iloc[i]['Netgen\nJanuary'])
-                monthly_energy.iloc[new_counter, 12:23] += monthly_energy_full.iloc[i, 12:23]
+                monthly_energy.iloc[new_counter, 12:24] += monthly_energy_full.iloc[i, 12:24]
 
+        print('dataframe ready! ' + str(datetime.datetime.now().time()))
 
+        # test_df = test_df.groupby(['Plant Id'].sum())
+        # print(test_df.loc[:3, :])
 
-
-        #-----------------------------------------
-        # i = 0
-        # for plant in monthly_energy_full['Plant Id']:
-        #     if plant not in monthly_energy['Plant Id']:
-        #         new_row = monthly_energy_full[plant]
-        #         monthly_energy.loc[i] = new_row
-        #         i += 1
-        #     else:
-        #         monthly_energy[plant, 'Netgen\nJanuary'] += monthly_energy_full[plant, 'Netgen\nJanuary']
-        #
-        # # for plant in monthly_energy_full['Plant Id']:
-        # #     monthly_energy(plant, 'Plant Id') = monthly_energy_full(plant, 'Plant Id')
-        # #     monthly_energy(plant, 'Netgen\nJanuary') += monthly_energy_full.loc[plants[plant], 'Netgen\nJanuary']
         print(monthly_energy_full.loc[:10, :])
         print(monthly_energy.loc[:3, :])
-        # # print('Testing: what does monthly_energy_full[Plant Id] give? ' + monthly_energy_full['Plant Id'])
+
         # # remove columns for prime mover and fuel type (below)
         # monthly_energy = monthly_energy.drop(columns=['Reported\nPrime Mover', 'AER\nFuel Type Code'])
         return monthly_energy
-    #
-    # def setup_ferc_gross_load(self):
-    #     # this code loads FERC 714 demand data and produces a dataframe
-    #     # of hourly load data, columns for each respondent
-    #     temp_gross_load_df = pd.read_csv(self.data_path + '/Part 3 Schedule 2 - Planning Area Hourly Demand.csv')
-    #     temp_gross_load_df = temp_gross_load_df.ix[:, 0:31]
-    #     temp_gross_load_index = temp_gross_load_df.set_index('respondent_id')
-    #     all_ferc_ids = list(set(list(temp_gross_load_index.index.values)))
-    #     temp_gross_load_df = temp_gross_load_df.drop(['report_yr', 'report_prd',
-    #                                                   'spplmnt_num', 'row_num',
-    #                                                   'timezone'], axis=1)
-    #     temp_gross_load_df = temp_gross_load_df.set_index(['respondent_id', 'plan_date'])
-    #     gross_load_df = reshape_ferc(temp_gross_load_df, all_ferc_ids)
-    #     del temp_gross_load_df
-    #     max_load = gross_load_df.max(axis=0).T
-    #     return gross_load_df
-
